45412abeeb4230f274c5f97cb49d67226473e7db
Alex Potter <alex.potter@ghyston.com>
2019-11-29 11:52:37 +0000

    Upgrade packages

--- src/ValueValidationResult.ts
+++ src/ValueValidationResult.ts
@@ -1,15 +1,15 @@
 export type ValueValidationResult<TValue> =
   | (TValue extends Array<infer TEachValue>
       ? Array<ValueValidationResult<TEachValue>> | string | null
-      : (TValue extends object
-          ? (
-              | {
-                  [propertyName in keyof TValue]?: ValueValidationResult<
-                    TValue[propertyName]
-                  >;
-                }
-              | string
-              | null)
-          : string | null))
+      : TValue extends object
+      ?
+          | {
+              [propertyName in keyof TValue]?: ValueValidationResult<
+                TValue[propertyName]
+              >;
+            }
+          | string
+          | null
+      : string | null)
   | string
   | null;
